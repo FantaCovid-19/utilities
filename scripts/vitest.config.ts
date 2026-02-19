e5da59224057a700c0c275bf08b20462a0c2d83b
Mr.Fantasma <contact@mrfantasma.com>
2026-02-18 22:44:32 -0300

    feat(vitest): Adds vitest configuration

---
+++ scripts/vitest.config.ts
@@ -0,0 +1,31 @@
+import type { ESBuildOptions } from "vite";
+import { defineConfig, type ViteUserConfig } from "vitest/config";
+
+export function createVitestConfig(options: ViteUserConfig = {}) {
+	return defineConfig({
+		...options,
+		test: {
+			...options?.test,
+			globals: true,
+			coverage: {
+				...options?.test?.coverage,
+				provider: "v8",
+				enabled: true,
+				reporter: ["text", "lcov"],
+				exclude: [
+					...(options.test?.coverage?.exclude ?? []),
+					"**/node_modules/**",
+					"**/dist/**",
+					"**/tests/**",
+					"**/tsup.config.ts",
+					"**/vitest.config.ts",
+				],
+			},
+		},
+		esbuild: {
+			...options?.esbuild,
+			target:
+				(options?.esbuild as ESBuildOptions | undefined)?.target ?? "es2021",
+		},
+	});
+}
